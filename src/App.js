a7349f9ea9148d90a81b9c5225b76decf9b835c9
Ola Leier <olaleier@gmail.com>
2019-06-20 16:29:04 +0200

    Progress saved.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Survey from "./components/survey";
+import Plot from "./components/plot";
+import NavBar from "./components/navbar";
 
-function App() {
-  return <div>Hallo.</div>;
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <Container>
+        <NavBar />
+        <Row>
+          <Col xs={7}>
+            <Survey />
+          </Col>
+          <Col>
+            <Plot />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const NavBar = () => {
+  return <nav className="navbar-nav navbar-text">Welcome to my navbar</nav>;
+};
+
+export default NavBar;
