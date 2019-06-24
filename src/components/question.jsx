b797fd48d3e693d1711409feca53766cdfb2f8d9
Ola Leier <olaleier@gmail.com>
2019-06-24 13:34:03 +0200

    Progress saved.

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,32 +1,72 @@
 import React, { Component } from "react";
 
-const Question = ({ q }) => {
-  const alternatives = q.alternatives.map(a => (
-    <div key={a.key}>
-      <label className="form-check-label">
-        <input
-          className="form-check-input"
-          name="radiolist"
-          type="radio"
-          value={a.key}
-        />
-        {a.value}
-      </label>
-    </div>
-  ));
-  return (
-    <React.Fragment>
-      <p>{q.title}</p>
-      <form>
-        <div className="form-check">{alternatives}</div>
-      </form>
-      {q.hours && HourButtons()}
-    </React.Fragment>
-  );
-};
+class Question extends Component {
+  state = { inputHours: 1 };
+  constructor() {
+    super();
+    this.changeHours = this.changeHours.bind(this);
+  }
+  render() {
+    const { q } = this.props;
+    const alternatives = q.alternatives.map(a => (
+      <div key={a.key}>
+        <label className="form-check-label">
+          <input
+            className="form-check-input"
+            name="radiolist"
+            type="radio"
+            value={a.key}
+          />
+          {a.value}
+        </label>
+      </div>
+    ));
+    return (
+      <React.Fragment>
+        <p>{q.title}</p>
+        <form>
+          <div className="form-check">{alternatives}</div>
+        </form>
+        {q.hours && this.HourButtons(this.state.inputHours)}
+      </React.Fragment>
+    );
+  }
+  HourButtons() {
+    const style = "btn btn-outline-secondary ";
+    const hrs = this.state.inputHours;
+    return (
+      <div>
+        Travel time:
+        <br />
+        <button className={style} onClick={this.decHours}>
+          -
+        </button>
+        <button className={style + "w-25"}>
+          {hrs + " hour" + (hrs > 1 ? "s" : "")}
+        </button>
+        <button className={style} onClick={this.incHours}>
+          +
+        </button>
+      </div>
+    );
+  }
 
-const HourButtons = () => {
-  return <div>Hours was true</div>;
-};
+  incHours = () => {
+    this.changeHours(1);
+  };
+
+  decHours = () => {
+    this.changeHours(-1);
+  };
+
+  changeHours(change) {
+    const hrs = this.state.inputHours + change;
+    if (hrs > 0) {
+      this.setState({
+        inputHours: hrs
+      });
+    }
+  }
+}
 
 export default Question;
--- src/components/survey.jsx
+++ src/components/survey.jsx
@@ -3,14 +3,11 @@ import Question from "./question";
 
 class Survey extends Component {
   render() {
-    const buttonclass = "btn btn-";
     return (
       <React.Fragment>
         <h4>Question:</h4>
         <br />
         <Question q={this.props.questions[0]} />
-        <button className={buttonclass + "secondary"}>Previous</button>
-        <button className={buttonclass + "primary w-25 ml-1"}>Next</button>
       </React.Fragment>
     );
   }
