d91f2c1ba1a4963bc5e59665705eb5cf3959a4e9
Ola Leier <olaleier@gmail.com>
2019-06-25 15:33:55 +0200

    Removed Chart.js, not React compatible

--- src/components/plot.jsx
+++ src/components/plot.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import Chart from "chart.js";
 
 class Plot extends Component {
   render() {
     //if the survey is complete, draw the plot. If during a survey, plot info pics etc.
-    return <h4>A plot goes here</h4>;
+    return <span>This should be a chart</span>;
   }
 }
 
