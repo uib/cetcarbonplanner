5c5047aee1265ac7bf288fd8ef5b55a74e4f4b3c
Ola Leier <olaleier@gmail.com>
2019-07-03 13:20:44 +0200

    chart.js for react is useless

--- src/components/plot-reactvis.jsx
+++ src/components/plot-reactvis.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import {
-  XYPlot,
+  FlexibleWidthXYPlot,
   VerticalGridLines,
   HorizontalGridLines,
   XAxis,
@@ -24,25 +24,24 @@ class Plot extends Component {
 
   getTest() {
     const data = [
-      { x: 0, y: 8 },
-      { x: 1, y: 5 },
-      { x: 2, y: 4 },
-      { x: 3, y: 9 },
-      { x: 4, y: 1 },
-      { x: 5, y: 7 },
-      { x: 6, y: 6 },
-      { x: 7, y: 3 },
-      { x: 8, y: 2 },
-      { x: 9, y: 0 }
+      { x: "Aircraft", y: 8 },
+      { x: "A long data label", y: 15 },
+      { x: "Electric Car", y: 4 },
+      { x: "train", y: 9 }
     ];
     return (
-      <XYPlot height={300} width={300}>
+      <FlexibleWidthXYPlot
+        height={300}
+        width={300}
+        xType="ordinal"
+        margin={{ bottom: 50 }}
+      >
         <VerticalGridLines />
         <HorizontalGridLines />
         <XAxis />
         <YAxis />
         <VerticalBarSeries data={data} />
-      </XYPlot>
+      </FlexibleWidthXYPlot>
     );
   }
 }
