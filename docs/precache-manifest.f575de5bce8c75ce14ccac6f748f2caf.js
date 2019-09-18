081ef1283ac23e79543e11801fb91f3222aa60b3
Ola Leier <olaleier@gmail.com>
2019-09-18 22:33:44 +0200

    Added folder docs for Github pages

---
+++ docs/precache-manifest.f575de5bce8c75ce14ccac6f748f2caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8feca53998a587037639f7e59b370f73",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aefc0f9edd9b365d95e1",
+    "url": "./static/css/2.57797527.chunk.css"
+  },
+  {
+    "revision": "6215fdfc623a35f6dcc5",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "aefc0f9edd9b365d95e1",
+    "url": "./static/js/2.7b82af7f.chunk.js"
+  },
+  {
+    "revision": "6215fdfc623a35f6dcc5",
+    "url": "./static/js/main.d9eb9067.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "69c49c1a2599bbfb30950e5911f722f9",
+    "url": "./static/media/emissionsguide.69c49c1a.jpg"
+  }
+]);
